00b0902b6cdaf6ff94a3a0db859322e47a5986bd
Alvin Cabayan <alvincabayan@gmail.com>
2019-05-24 06:51:25 +1200

    [0001] Adding the OrderForm

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import OrderForm2 from './components/OrderForm2'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Container>
+        <OrderForm2></OrderForm2>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
